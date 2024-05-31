2441b33d0d7d06d325ee9b37d5ffe350d396ea3a
ivanvet31 <ivanvet31@gmail.com>
2024-05-31 20:30:55 +0000

    implemented file browser as class fixed2

--- main.cpp
+++ main.cpp
@@ -28,17 +28,19 @@ fact(__uint128_t a)
 int
 main()
 {
+  char ch;
+
   std::string path = dir_parser::getPath();
 
   path = dir_parser::browseFiles(path);
   Translation algo(path, std::vector<std::string>{ "p" });
 
-  int it = 1;
+  /*int it = 1;
   for (const auto &f : algo.getFuncs())
     {
       std::cout << "the " << it << " func" << " \n=============================\n" << f << "\n";
       ++it;
-    }
+    }*/
 
   algo.getComplexity();
 
@@ -46,6 +48,7 @@ main()
     {
       std::cout << "func 1: \n\t" << algo.getComplexity(i) << '\n';
     }
+
 
   //todo();
 }
