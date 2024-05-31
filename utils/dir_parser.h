b31b6288df07f18c23b947313d65d86ed516fc41
ivanvet31 <ivanvet31@gmail.com>
2024-05-31 20:18:44 +0000

    implemented file browser as class fixed

--- utils/dir_parser.h
+++ utils/dir_parser.h
@@ -5,13 +5,13 @@ class dir_parser
 {
 public:
   /* возвращает вектор директорий и файлов, находящихся по абсолютному пути 'path'*/
-  static inline std::vector<std::string> getDirs(const std::string &path);
+  static std::vector<std::string> getDirs(const std::string &path);
   /* возвращает директорию, из которой была запущена программа */
-  static inline std::string getPath();
+  static std::string getPath();
 
   /* вывод директорий */
-  static inline void printVec(const std::vector<std::string> &vec);
+  static void printVec(const std::vector<std::string> &vec);
 
   /* файловый поисковик */
-  static inline std::string browseFiles(const std::string &path);
+  static std::string browseFiles(const std::string &path);
 };
